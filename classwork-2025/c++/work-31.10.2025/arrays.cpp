7501fe78312b291b559380802ca6343a40b1a843
Wenwu <artemiip427@gmail.com>
2025-10-31 13:48:28 +0300

    classwork

---
+++ classwork-2025/c++/work-31.10.2025/arrays.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <limits>
+
+int main(){
+    const int rowCount{8};
+    const int columnCount{8};
+    
+    int arr[rowCount][columnCount]{
+        {5,4,3,2,1,3,4},
+        {2,6,565,76,7,6,6},
+        {0,2,0,78,0,68,0},
+        {0,68,0,0,8,0,0},
+        {0,3,0,0,9,0,0},
+        {0,0,9,0,0,0,0},
+        {0,0,0,0,0,79,0},
+        {8,6,97,6,6,726,676},
+    };
+
+    for (int r{};r<rowCount;r++){
+        int max{std::numeric_limits<int>::min()};
+        for (int i{0}; i < columnCount; i++)
+        {
+            if (arr[r][i]>max){
+                max = arr[r][i];
+            }
+        }
+        std::cout << r+1 << "\t" << max << "\n";
+        
+    }
+
+  return 0;
+}
