45f5bf365120e7d1023ae756a6538c0cffc7b531
Wenwu <artemiip427@gmail.com>
2025-11-03 23:46:50 +0300

    homework

--- homework-2025/основы бд/homework-03.11/script.sql
+++ homework-2025/основы бд/homework-03.11/script.sql
@@ -10,32 +10,33 @@ GROUP BY rating
 ORDER BY film_count DESC;
 
 -- 3)
-SELECT p.customer_id, SUM(p.amount) AS total_amount
-FROM payment p
-JOIN customer c ON p.customer_id = c.customer_id
-GROUP BY p.customer_id
-ORDER BY p.customer_id;
+SELECT customer_id, 
+(SELECT SUM(amount)FROM payment p2 WHERE p2.customer_id = p1.customer_id) AS total_amount
+FROM payment p1
+GROUP BY customer_id
+ORDER BY customer_id;
 
 -- 4)
-SELECT MONTH(rental_date) AS month, COUNT(rental_id) AS rental_count
+SELECT YEAR(rental_date) AS year, MONTH(rental_date) AS month, COUNT(rental_id) AS rental_count
 FROM rental
-GROUP BY MONTH(rental_date);
+GROUP BY YEAR(rental_date), MONTH(rental_date);
 
 -- 5)
-SELECT rating, COUNT(film_id) AS film_count
-FROM film
-GROUP BY rating
-HAVING COUNT(film_id) > 200;
+SELECT rating, film_count
+FROM 
+(SELECT rating, COUNT(film_id) AS film_count FROM film GROUP BY rating) AS rating_counts
+WHERE film_count > 200;
 
 -- 6)
-SELECT p.customer_id, SUM(p.amount) AS total_amount
-FROM payment p
-JOIN customer c ON p.customer_id = c.customer_id
-GROUP BY p.customer_id
-HAVING SUM(p.amount) > 150
-ORDER BY p.customer_id;
+SELECT customer_id, total_amount
+FROM 
+(SELECT customer_id, SUM(amount) AS total_amount FROM payment GROUP BY customer_id) AS customer_totals
+WHERE total_amount > 150
+ORDER BY customer_id;
 
 -- 7)
 SELECT first_name, last_name
 FROM customer
-WHERE city = 'London';
+WHERE address_id IN 
+(SELECT address_id FROM address WHERE city_id IN 
+(SELECT city_id FROM city WHERE city = 'London'));
