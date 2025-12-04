f82882a4b75a71dd426ee275a2576ea03c3ed776
Wenwu <artemiip427@gmail.com>
2025-12-04 15:01:16 +0300

    classwork in c**

--- classwork-2025/c++/work-04.12.2025/task-3.cpp
+++ classwork-2025/c++/work-04.12.2025/task-3.cpp
@@ -1,15 +1,41 @@
 #include <iostream>
 
 void printArr(int arr[],int len);
+void swap(int* x, int* y);
+void swap2(int &x, int &y);
 
 int main(){
     const int len{5};
 
     int arr[len]{1,0,-5,34,6};
+    //printArr(arr,len);
 
-    printArr(arr,len);
+    int a{5};
+    int b{8};
+
+    std::cout << a << " " << b << "\n";
+    swap (&a,&b);
+    std::cout << "swap 1 - " << a << " " << b << "\n";
+    swap2 (a,b);
+    std::cout << "swap 2 - " << a << " " << b << "\n";
+
+    
+
 
     return 0;
+}
+
+void swap(int* x,int* y){
+    int t{*x};
+    *x = *y;
+    *y = t;
+
+}
+void swap2(int &x,int &y){
+    int t{x};
+    x = y;
+    y = t;
+
 }
 
 void printArr(int* arr,int len)
