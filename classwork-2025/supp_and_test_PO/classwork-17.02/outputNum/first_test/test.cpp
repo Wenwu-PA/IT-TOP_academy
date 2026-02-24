e137a1f5d0a649fda04a1c1e25ed6821de9f5136
Wenwu <artemiip427@gmail.com>
2026-02-24 08:05:02 +0300

    add classwork

---
+++ classwork-2025/supp_and_test_PO/classwork-17.02/outputNum/first_test/test.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "../outputNum/lib.cpp"
+
+TEST(TestCaseName, TestCase1Name) {
+	EXPECT_EQ(compare(5, 4), 1);
+	EXPECT_TRUE(true);
+
+}
+TEST(TestCaseName, TestCase2Name) {
+	EXPECT_EQ(compare(4, 2), 1);
+	EXPECT_TRUE(true);
+}
+TEST(TestCaseName, TestCase3Name) {
+	EXPECT_EQ(compare(0, -5), 1);
+	EXPECT_TRUE(true);
+}
---
+++ classwork-2025/supp_and_test_PO/classwork-17.02/outputNum/outputNum/lib.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+int compare(int x, int y)
+{
+    if (x > y)
+        return 1;
+    //std::cout << x << " is greater than " << y << '\n'; // case 1
+    else if (x < y)
+        ///std::cout << x << " is less than " << y << '\n'; // case 2
+        return -1;
+    else
+        //std::cout << x << " is equal to " << y << '\n'; // case 3
+        return 0;
+
+}
