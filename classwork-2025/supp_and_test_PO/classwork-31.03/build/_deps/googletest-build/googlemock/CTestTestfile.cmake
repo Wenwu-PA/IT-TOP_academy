51c07c2562879db8b9e50ff2bd7b71df8caecfd1
Wenwu <artemiip427@gmail.com>
2026-03-31 13:47:49 +0300

    cv

--- classwork-2025/supp_and_test_PO/classwork-31.03/build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/git/IT-TOP_academy/classwork-2025/supp_and_test_PO/classwork-31.03/build/_deps/googletest-src/googlemock
-# Build directory: D:/git/IT-TOP_academy/classwork-2025/supp_and_test_PO/classwork-31.03/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
